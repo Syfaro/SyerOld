65de30dce6fcd045886d6fce5c63f77c00263404
Syfaro Warraw <syfaro@me.com>
2013-07-30 15:10:55 -0500

    first of seen module

---
+++ modules/seen.js
@@ -0,0 +1,26 @@
+var LogLine = {
+	Name: 'Line logger',
+	Command: {
+		Type: 'regex',
+		regex: /.+/
+	},
+	Run: {
+		
+	},
+	Help: {
+		Text: 'Logs a line',
+		Example: 'n/a'
+	},
+	RunFunction: function(trigger, helpers, irc, callback) {
+		syer.SQL('chanlogs')
+			.insert({
+				channel: trigger.to,
+				nick: trigger.from,
+				host: trigger.rawInfo.host,
+				message: trigger.message
+			})
+			.exec();
+	}
+};
+
+RegisterCommand(LogLine);
